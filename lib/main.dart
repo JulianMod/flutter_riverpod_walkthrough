1f063621c97e8b0e20fbdf920c14285cefaefe59
JuliaMod <julmodlinski@gmail.com>
2022-12-23 21:32:19 +0100

    add counter page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final counterProvider = StateProvider((ref) => 0);
+abstract class WebsocketClient {
+  Stream<int> getCounterStream([int start]);
+}
+
+class FakeWebsocketClient implements WebsocketClient{
+  @override
+  Stream<int> getCounterStream([int start = 0]) async* {
+    int i = start;
+    while (true) {
+      yield i++;
+      await Future.delayed(const Duration(milliseconds: 500));
+    }
+  }
+}
+
+final websocketClientProvider = Provider<WebsocketClient>((ref) {
+  return FakeWebsocketClient();
+});
+
+final counterProvider = StreamProvider.family<int, int>((ref, start) {
+  final wsClient = ref.watch(websocketClientProvider);
+  return wsClient.getCounterStream(start);
+},
+);
 
 void main() {
   runApp(
@@ -19,13 +42,61 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Counter App',
-      home: Container(),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.green,
           brightness: Brightness.dark,
           surface: const Color(0xff003909),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go to Counter Page'),
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: ((context) => const CounterPage()))
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CounterPage extends ConsumerWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<int> counter = ref.watch(counterProvider(5));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: Text(
+          counter.when(
+            data: (int value) => value,
+            error: (Object e, _) => e,
+            loading: () => 5,
+          ).toString(),
+          style: Theme.of(context).textTheme.displayMedium,
         ),
       ),
     );
